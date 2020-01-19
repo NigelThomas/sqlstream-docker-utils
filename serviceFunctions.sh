73d5fb5ca5349d5af258871ca3a7a6010a4fe778
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-19 16:58:02 +0000

    missing dquote

--- serviceFunctions.sh
+++ serviceFunctions.sh
@@ -135,7 +135,7 @@ function getGitProject()
 {
     # the environment variables define which version of which project gets loaded
 
-    if [ -z "$GIT_PROJECT_HASH" -o "$GIT_PROJECT_HASH" = "HEAD ]
+    if [ -z "$GIT_PROJECT_HASH" -o "$GIT_PROJECT_HASH" = "HEAD" ]
     then
         echo ... getting latest for $GIT_PROJECT_NAME
         git clone --depth 1 ${GIT_ACCOUNT}/${GIT_PROJECT_NAME}.git
