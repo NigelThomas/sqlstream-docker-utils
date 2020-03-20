f2dfece72e59ebf6a50698f71d6fe29bc8c12078
NigelThomas <nigel.cl.thomas@gmail.com>
2020-03-20 12:34:37 +0000

    refactor to inline streamlab_startup

--- load_streamlab_projects.sh
+++ load_streamlab_projects.sh
@@ -30,8 +30,38 @@ preStartup
 
 # the startup script orchestrates loading and running the project
 
-echo launching startup_streamlab
-. startup_streamlab.sh
+# loads all slab files into StreamLab, and starts underlying pumps
+# assume PATH is set to ensure we can find subsidiary scripts
+# assume cwd is set to the project directory
+
+echo Loading StreamLab projects from `pwd` using $LOAD_SLAB_FILES
+
+for f in ${LOAD_SLAB_FILES}
+do
+    echo loading slab files $f from `pwd`
+
+    if [ -e $f ]
+    then
+        echo
+        echo posting $f to StreamLab
+
+        # TODO check if project already exists in repository, fail if so
+
+        # convert the SLAB file into a submission
+        python /home/sqlstream/sqlstream-docker-utils/slabLoader.py $f
+
+        # call the API
+        curl -v -H "Content-Type: application/json" -d@/tmp/slab.json http://localhost:5585/_project_import/user
+
+        # TODO check response
+
+        rm /tmp/slab.json
+        echo
+
+    else
+        echo No such project export file $f
+    fi
+done
 
 time PROJECT_NAME=GIT_PROJECT_NAME PATH=$PATH startup.sh
 echo "========= Ready ======="
