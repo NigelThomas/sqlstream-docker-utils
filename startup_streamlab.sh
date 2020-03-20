25b319ba0cb4f5f4c6554f5f1ac55fba9294f16d
NigelThomas <nigel.cl.thomas@gmail.com>
2020-03-20 10:23:51 +0000

    inline the importSlabFiles function

--- startup_streamlab.sh
+++ startup_streamlab.sh
@@ -9,7 +9,8 @@
 echo startup_streamlab.sh PATH=$PATH
 
 . serviceFunctions.sh
-. streamlabFunctions.sh
+
+startStreamLab
 
 echo Loading StreamLab projects from `pwd`
 
@@ -17,7 +18,31 @@ echo Loading StreamLab projects from `pwd`
 ls -l
 
 # load all slab files
-echo call importSlabFiles
-importSlabFiles
 
+echo loading slab files $LOAD_SLAB_FILES from `pwd`
+
+for f in ${LOAD_SLAB_FILES:=*.slab}
+do
+    if [ -e $f ]
+    then
+        echo
+        echo posting $f to StreamLab
+
+        # TODO check if project already exists in repository, fail if so
+
+        # convert the SLAB file into a submission
+        python /home/sqlstream/sqlstream-docker-utils/slabLoader.py $f
+
+        # call the API
+        curl -v -H "Content-Type: application/json" -d@/tmp/slab.json http://localhost:5585/_project_import/user
+
+        # TODO check response
+
+        rm /tmp/slab.json
+        echo
+
+    else
+        echo No such project export file $f
+    fi
+done
 
