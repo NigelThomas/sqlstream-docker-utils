4ae26aecb3fff42dd8a23bf07b42ff3ae6f7bdd3
NigelThomas <nigel.cl.thomas@gmail.com>
2020-06-06 15:09:31 +0100

    add period rows to tnodestream

--- stats/tnodestream.sql
+++ stats/tnodestream.sql
@@ -1,7 +1,10 @@
 select stream measured_at,node_id,graph_id,sched_state,trim(last_exec_result) as last_exec_result
      ,input_rowtime_clock,net_input_bytes, net_input_rows
+     , net_input_rows - first_value(net_input_rows) OVER "2rows" as increment_input_rows
      ,output_rowtime_clock,net_output_bytes, net_output_rows
-     ,execution_count,net_execution_time
-from stream(sys_boot.mgmt.getStreamOperatorInfoForever(0,60))
-where name_in_qiery_plan like '[LOCAL%'
+     , net_output_rows - first_value(net_output_rows) OVER "2rows" as increment_output_rows
+     ,execution_count,net_execution_time,name_in_query_plan as stream_name
+from stream(sys_boot.mgmt.getStreamOperatorInfoForever(0,60)) s
+window "2rows" as (partition by name_in_query_plan rows 1 preceding)
+where name_in_query_plan like '[LOCAL%'
 ;
